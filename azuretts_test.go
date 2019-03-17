99c2e8afffa4e0048909eafdda547525f33009b3
jesseward <jesse.ward@gmail.com>
2019-03-17 09:26:22 -0400

    Additional test coverage and travis integration.

--- azuretts_test.go
+++ azuretts_test.go
@@ -16,6 +16,26 @@ func TestVoiceXML(t *testing.T) {
 	assert.Equal(t, expect, voiceXML("Microsoft Speech Service Text-to-Speech API", description, region, gender))
 }
 
+func TestSynthesize(t *testing.T) {
+	az := &AzureCSTextToSpeech{SubscriptionKey: "SYS64738", accessToken: "SYS49152"}
+
+	// payload should be nil and err should be true, since DeCH + Female is not a valid combination
+	payload, err := az.Synthesize("test-speech", DeCH, Female, RIFF8Bit8kHzMonoPCM)
+	assert.Error(t, err, "should raise an error")
+	assert.Nil(t, payload, "payload should be nil")
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("SYS4096"))
+		}),
+	)
+	defer ts.Close()
+	az.APIBase = ts.URL
+	payload, err = az.Synthesize("SYS4096", EnUS, Female, RIFF8Bit8kHzMonoPCM)
+	assert.NoError(t, err)
+	assert.Equal(t, payload, []byte("SYS4096"))
+}
+
 // TestRefreshToken validates logic for fetching of the RefreshToken
 func TestRefreshToken(t *testing.T) {
 	az := &AzureCSTextToSpeech{SubscriptionKey: "ThisIsMySubscriptionKeyAndToBeToken"}
@@ -31,6 +51,6 @@ func TestRefreshToken(t *testing.T) {
 	az.tokenAPI = ts.URL
 	err := az.RefreshToken()
 
-	assert.NoError(t, err)
-	assert.Equal(t, az.SubscriptionKey, az.accessToken)
+	assert.NoError(t, err, "should not return an error")
+	assert.Equal(t, az.SubscriptionKey, az.accessToken, "values should be equal")
 }
