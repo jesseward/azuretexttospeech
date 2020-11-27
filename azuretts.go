48c29995528af4dc66c182ac4dbda3a48d65b7c5
Jesse Ward <jeward@linkedin.com>
2020-11-27 07:30:33 -0800

    minor refactor and migartion to github actions

--- azuretts.go
+++ azuretts.go
@@ -2,6 +2,7 @@ package azuretexttospeech
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -20,35 +21,61 @@ const tokenRefreshTimeout = time.Second * 15
 
 // TextToSpeechAPI references the locations of the availability of standard voices.
 // See https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/regions#standard-voices
-type TextToSpeechAPI string
+type TextToSpeechAPI int
 
 const (
 	// Azure regions and their endpoints that support the Text To Speech service.
-	WestUS        TextToSpeechAPI = "https://westus.tts.speech.microsoft.com"
-	WestUS2       TextToSpeechAPI = "https://westus2.tts.speech.microsoft.com"
-	EastUS        TextToSpeechAPI = "https://eastus.tts.speech.microsoft.com"
-	EastUS2       TextToSpeechAPI = "https://eastus2.tts.speech.microsoft.com"
-	EastAsia      TextToSpeechAPI = "https://eastasia.tts.speech.microsoft.com"
-	SoutheastAsia TextToSpeechAPI = "https://southeastasia.tts.speech.microsoft.com"
-	NorthEurpoe   TextToSpeechAPI = "https://northeurope.tts.speech.microsoft.com"
-	WestEurope    TextToSpeechAPI = "https://westeurope.tts.speech.microsoft.com"
+	WestUS TextToSpeechAPI = iota
+	WestUS2
+	EastUS
+	EastUS2
+	EastAsia
+	SoutheastAsia
+	NorthEurpoe
+	WestEurope
 )
+
+func (t TextToSpeechAPI) String() string {
+	return fmt.Sprintf("https://%s.tts.speech.microsoft.com", [...]string{
+		"westus",
+		"westus2",
+		"eastus",
+		"eastus2",
+		"eastasia",
+		"southeastasia",
+		"northeurope",
+		"westeurope",
+	}[t])
+}
 
 // TokenRefreshAPI references the Azure endpoints required to fetch the bearer token.
 // https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-apis#how-to-get-an-access-token
-type TokenRefreshAPI string
+type TokenRefreshAPI int
 
 const (
 	// 	Endpoints for token refresh/assignment.
-	WestUSToken        TokenRefreshAPI = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	WestUS2Token       TokenRefreshAPI = "https://westus2.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	EastUSToken        TokenRefreshAPI = "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	EastUS2Token       TokenRefreshAPI = "https://eastus2.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	EastAsiaToken      TokenRefreshAPI = "https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	SoutheastAsiaToken TokenRefreshAPI = "https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	NorthEuropeToken   TokenRefreshAPI = "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-	WestEuropeToken    TokenRefreshAPI = "https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+	WestUSToken TokenRefreshAPI = iota
+	WestUS2Token
+	EastUSToken
+	EastUS2Token
+	EastAsiaToken
+	SoutheastAsiaToken
+	NorthEuropeToken
+	WestEuropeToken
 )
+
+func (t TokenRefreshAPI) String() string {
+	return fmt.Sprintf("https://%s.api.cognitive.microsoft.com/sts/v1.0/issueToken", [...]string{
+		"westus",
+		"westus2",
+		"eastus",
+		"eastus2",
+		"eastasia",
+		"southeastasia",
+		"northeurope",
+		"westeurope",
+	}[t])
+}
 
 // serviceNameMappingString is a text placeholder that is present in the standard voices service name mapping. This is
 // used to build the `name` attribute in the ssml/xml payload (see `voiceXML()`).
@@ -62,18 +89,18 @@ func voiceXML(speechText, description string, locale Region, gender Gender) stri
 		locale, locale, gender, serviceNameMappingString, description, speechText)
 }
 
-// Synthesize returns a bytestream of the rendered text-to-speech in the target audio format. `speechText` is the string of
+// SynthesizeWithContext returns a bytestream of the rendered text-to-speech in the target audio format. `speechText` is the string of
 // text in which a user wishes to Synthesize, `region` is the language/locale, `gender` is the desired output voice
 // and `audioOutput` captures the audio format.
-func (az *AzureCSTextToSpeech) Synthesize(speechText string, region Region, gender Gender, audioOutput AudioOutput) ([]byte, error) {
+func (az *AzureCSTextToSpeech) SynthesizeWithContext(ctx context.Context, speechText string, region Region, gender Gender, audioOutput AudioOutput) ([]byte, error) {
 
-	descriprtion, ok := localeToGender[localeGender{region, gender}]
+	description, ok := localeToGender[localeGender{region, gender}]
 	if !ok {
 		return nil, fmt.Errorf("unable to locale region=%s, gender=%s pair", region, gender)
 	}
 
-	v := voiceXML(speechText, descriprtion, region, gender)
-	request, err := http.NewRequest(http.MethodPost, az.APIBase, bytes.NewBufferString(v))
+	v := voiceXML(speechText, description, region, gender)
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, az.APIBase, bytes.NewBufferString(v))
 	if err != nil {
 		return nil, err
 	}
@@ -82,8 +109,8 @@ func (az *AzureCSTextToSpeech) Synthesize(speechText string, region Region, gend
 	request.Header.Set("Authorization", "Bearer "+az.accessToken)
 	request.Header.Set("User-Agent", "azuretts")
 
-	client := &http.Client{Timeout: synthesizeActionTimeout}
-	response, err := client.Do(request)
+	client := &http.Client{}
+	response, err := client.Do(request.WithContext(ctx))
 	if err != nil {
 		return nil, err
 	}
@@ -93,6 +120,13 @@ func (az *AzureCSTextToSpeech) Synthesize(speechText string, region Region, gend
 		return nil, fmt.Errorf("unexpected status code; status=%s", response.Status)
 	}
 	return ioutil.ReadAll(response.Body)
+}
+
+// Synthesize directs to SynthesizeWithContext
+func (az *AzureCSTextToSpeech) Synthesize(speechText string, region Region, gender Gender, audioOutput AudioOutput) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), synthesizeActionTimeout)
+	defer cancel()
+	return az.SynthesizeWithContext(ctx, speechText, region, gender, audioOutput)
 }
 
 // RefreshToken fetches an updated token from the Azure cognitive speech/text services, or an error if unable to retrive.
@@ -154,9 +188,10 @@ type AzureCSTextToSpeech struct {
 func New(subscriptionKey string, api TextToSpeechAPI, token TokenRefreshAPI) (*AzureCSTextToSpeech, error) {
 
 	az := &AzureCSTextToSpeech{
-		APIBase:         string(api) + v1Path,
+		APIBase:         fmt.Sprintf("%s%s", api, v1Path),
 		SubscriptionKey: subscriptionKey,
-		tokenAPI:        string(token)}
+		tokenAPI:        fmt.Sprintf("%s", token),
+	}
 
 	// api requires that the token is refreshed every 10 mintutes.
 	// We will do this task in the background every ~9 minutes.
--- example/example.go
+++ example/example.go
@@ -1,41 +1,52 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"os"
 
 	tts "github.com/jesseward/azuretexttospeech"
 )
 
+func exit(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %+v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
 func main() {
 	// create a key for "Cognitive Services" (kind=SpeechServices). Once the key is available
 	// in the Azure portal, push it into an environment variable (export AZUREKEY=SYS64738).
 	// By default the free tier keys are served out of West US2
 	var apiKey string
 	if apiKey = os.Getenv("AZUREKEY"); apiKey == "" {
-		panic("Please set your AZUREKEY environment variable")
+		exit(fmt.Errorf("Please set your AZUREKEY environment variable"))
 	}
-	az, err := tts.New(apiKey, tts.WestUS2, tts.WestUS2Token)
+	az, err := tts.New(apiKey, tts.EastUS, tts.EastUSToken)
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
 	defer close(az.TokenRefreshDoneCh)
 
 	// Digitize a text string using the enUS locale, female voice and specify the
 	// audio format of a 16Khz, 32kbit mp3 file.
-	b, err := az.Synthesize(
+	ctx := context.Background()
+	b, err := az.SynthesizeWithContext(
+		ctx,
 		"64 BASIC BYTES FREE. READY.",
 		tts.EnUS,
 		tts.Female,
 		tts.Audio16khz32kbitrateMonoMp3)
 
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
+
 	// send results to disk.
 	err = ioutil.WriteFile("audio.mp3", b, 0644)
 	if err != nil {
-		panic(err)
+		exit(err)
 	}
-
 }
